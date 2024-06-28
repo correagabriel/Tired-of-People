578de8c73cd6e7bc8aaee0039b2b405eb3804965
Joszi <dasilvacorrejosegabriel@gmail.com>
2024-06-27 21:17:18 -0300

    i love my sogra

---
+++ login/js/javascript.js
@@ -0,0 +1,9 @@
+function aviso(){
+    if(formulario.username.value == ""){
+        alert("Campo Usuario Obrigatorio");
+    }
+    
+    if(formulario.password.value == ""){
+        alert("Campo Senha Obrigatorio");
+    }
+}
